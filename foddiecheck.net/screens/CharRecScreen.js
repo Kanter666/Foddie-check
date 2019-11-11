ad99b26b2e40e822393d99ed114ba5c145abbd16
codingmachine <kantorikadam@gmail.com>
2019-11-11 15:05:44 +0800

    Camera permission for CharRecScreen

--- foddiecheck.net/screens/CharRecScreen.js
+++ foddiecheck.net/screens/CharRecScreen.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  Button,
-  View
-} from 'react-native';
+import { Text, Alert, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
 
 
 class FirstScreen extends React.Component
@@ -15,16 +11,55 @@ class FirstScreen extends React.Component
       title: 'Character Recognition Screen'
    };
 
-   render()
-   {
-      return(
-         <View>
-            <Text>
-               Welcome to hell.
-            </Text>
+   state = {
+     hasCameraPermission: null,
+     type: Camera.Constants.Type.back,
+   };
 
-         </View>
-      );
+   async componentDidMount() {
+     const { status } = await Permissions.askAsync(Permissions.CAMERA);
+     this.setState({ hasCameraPermission: status === 'granted' });
    }
-}
+
+   render() {
+     const { hasCameraPermission } = this.state;
+     if (hasCameraPermission === null) {
+       return <View />;
+     } else if (hasCameraPermission === false) {
+       return <Text>No access to camera</Text>;
+     } else {
+       return (
+         <View style={{ flex: 1 }}>
+           <Camera style={{ flex: 1 }} type={this.state.type}>
+             <View
+               style={{
+                 flex: 1,
+                 backgroundColor: 'transparent',
+                 flexDirection: 'row',
+               }}>
+               <TouchableOpacity
+                 style={{
+                   flex: 0.1,
+                   alignSelf: 'flex-end',
+                   alignItems: 'center',
+                 }}
+                 onPress={() => {
+                   this.setState({
+                     type:
+                       this.state.type === Camera.Constants.Type.back
+                         ? Camera.Constants.Type.front
+                         : Camera.Constants.Type.back,
+                   });
+                 }}>
+                 <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+               </TouchableOpacity>
+             </View>
+           </Camera>
+         </View>
+       );
+     }
+   }
+ }
+
+
 export default FirstScreen;
